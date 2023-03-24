2279b7ab7362e5cc803bbd82250901976ef6279d
ZachC204 <96530968+ZachC204@users.noreply.github.com>
2023-03-24 13:37:16 -0400

    initial commit

--- Assets/scripts/MoveForward.cs
+++ Assets/scripts/MoveForward.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MoveForward : MonoBehaviour
 {
     public float speed;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
+        if(playerControllerScript.gameOver == true){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
     }
 }
